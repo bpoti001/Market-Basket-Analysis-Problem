2373a35aec12fdbad1a468028dc22dc7fa7dd17f
bpoti001 <bpoti001@users.noreply.github.com>
2016-04-27 20:59:00 -0400

    Update fre_item_map_ph2.py

--- Hadoop_Source/fre_item_map_ph2.py
+++ Hadoop_Source/fre_item_map_ph2.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 import sys
-sys.path.append('.')
-a=[]
-val=[]
-for  i in sys.stdin:
-        i = i.strip()
-        items = i.split()
-        a.append(items)
-a = map(set,a)
-for line in open('valA','r'):
-    new_line = line.strip()
-    if new_line:
-            new_line=new_line.split()
-            val.append(new_line)
-val = map(frozenset,val)
-for i in a:
-    for j in val:
-            if j.issubset(i):
-                    x,y=j
-                    print "(%s %s),1"%(x,y)
+datasets1 = []
+for line1 in sys.stdin:
+    line1 = line1.strip( )
+    items1 = line1.split(' ')
+    datasets1.append(items1)
+datasets1= map(set,datasets1)
+
+
+datasets2 = []
+#reduce1_output=open('reduce1out','r')
+for line2 in open('part-00000','r'):
+    line2 = line2.strip( )
+    if line2:
+        items2 = line2.split(' ')  
+        datasets2.append(items2)
+datasets2= map(frozenset,datasets2)
+
+for i in datasets1:
+    for j in datasets2:
+        if j.issubset(i):
+            print "%s\t%d" % (j,1)
